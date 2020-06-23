8c8da5bcad53e9f6bf75dcb2fbb2a500b3b23325
Kenneth V. Domingo <kvdomingo@up.edu.ph>
2020-06-24 04:18:29 +0800

    finish initialization

--- frontend/static/frontend/js/components/App.js
+++ frontend/static/frontend/js/components/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Image } from 'cloudinary-react';
 
 
 export default function App() {
     return (
-        <div className='container'>
-            <h1>Hello, React!</h1>
+        <div className='Container'>
+            <div className='App'>
+                <Image
+                    cloudName='kdphotography-assets'
+                    className='Logo'
+                    publicId='ipl/IPL_Logo'
+                    responsive
+                    responsiveUseBreakpoints
+                    width='auto'
+                    dpr='auto'
+                    crop='scale'
+                    />
+                <h1>Coming soon</h1>
+            </div>
         </div>
     );
 }
